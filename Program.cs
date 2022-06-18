79b1c7327171762881b0592aeff15ffa4d7e9407
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-06-19 01:20:56 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Сумма чисел от 1 до А.
+
+int Func(int num)
+{
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Func(A));
